64706929d113d7a7885b8c47faa93d95da970d81
JoaoAlgeri <joaoalgeri@gmail.com>
2022-11-21 15:12:05 -0300

    versao componentizada

---
+++ src/Components/Sugestoes.js
@@ -0,0 +1,14 @@
+import Sugestao from "./Sugestao";
+export default function Sugestoes() {
+    return (
+        <>
+            <div className="sugestoes">
+                <div className="titulo">
+                    Sugestões para você
+                    <div>Ver tudo</div>
+                </div>
+                <Sugestao />
+            </div>
+        </>)
+
+}
